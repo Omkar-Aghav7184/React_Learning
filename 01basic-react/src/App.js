28a47c83624cca23e8674a1cc8f5460f06e07142
Omkar <omkar.t.aghav@gmail.com>
2025-08-04 11:43:52 +0530

    Create First React Project

---
+++ 01basic-react/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>React with Omkar Champion!</h1>
+  );
+}
+
+export default App;
---
+++ 01vite-react/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+  
+  return (
+   <h1>Hellow Learn React + Vite with Omkar Champion!</h1>
+  )
+}
+
+export default App
