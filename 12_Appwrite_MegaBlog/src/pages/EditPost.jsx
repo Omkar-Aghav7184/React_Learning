fddf37918a76669f9655be3fc4074beae46eadce
Omkar <omkar.t.aghav@gmail.com>
2025-08-19 11:35:59 +0530

    adds EditPost

---
+++ 12_Appwrite_MegaBlog/src/pages/EditPost.jsx
@@ -0,0 +1,34 @@
+import React,{useEffect,useState} from 'react'
+import { Container,PostForm } from '../components'
+import dataBaseService from '../appWrite/database'
+import { useNavigate,useParams } from 'react-router-dom'
+
+function EditPost() {
+
+    const [post,setPosts] = useState(null)
+    const {slug} = useParams()
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        if(slug){
+            dataBaseService.getAllPosts(slug).then((post)=>{
+                if(post){
+                    setPosts(post)
+                }
+            })
+        }
+        else{
+            navigate('/')
+        }
+    },[slug,navigate])
+
+    return post?(
+        <div className='py-8'>
+            <Container>
+                <PostForm post={post}/>
+            </Container>
+        </div>
+    ): null
+}
+
+export default EditPost
