6f44fae8846d3327d6f4d3db4abab09e56a3ba4d
Omkar <omkar.t.aghav@gmail.com>
2025-08-19 11:36:30 +0530

    adds Post

---
+++ 12_Appwrite_MegaBlog/src/pages/Post.jsx
@@ -0,0 +1,14 @@
+import React,{useEffect,useState} from 'react'
+import { Link,useNavigate,useParams } from 'react-router-dom'
+import dataBaseService from '../appWrite/database'
+import { Button,Container } from '../components'
+import parse from 'html-react-parser'
+import { useSelector } from 'react-redux'
+
+function Post() {
+    return (
+        <div>Post</div>
+    )
+}
+
+export default Post
